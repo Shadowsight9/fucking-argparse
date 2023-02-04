cb01b323ba7c4d797427fd5332e18674ef48c20b
huangzheng <huangzheng@shu.edu.cn>
2023-02-04 21:49:59 +0800

    tests: add more types for test

--- tests/arguments.py
+++ tests/arguments.py
@@ -1,11 +1,19 @@
+from pathlib import PosixPath
+from torch import device
+from torch.nn.modules.linear import Linear
 from dataclasses import dataclass
 
 
 @dataclass()
 class Arguments:
-    inductor: str = "rule"
-    group_beam: bool = False
-    mlm_training: bool = False
-    bart_training: bool = False
-    if_then: bool = False
-    task: str = "openrule155"
+    test_str: str = "rule"
+    test_bool: bool = False
+    test_int: int = 123
+    test_float: float = 3.1
+    test_complex: complex = (1+2j)
+    test_list: list = [1, 2.1, 'hello', False]
+    test_set: set = {False, 1, 2.1, 'hello'}
+    test_dict: dict = {'a': 1, 'b': 2.1, 'c': 'hello', 'd': False}
+    test_path: PosixPath = PosixPath('.')
+    test_device: device = device(type='cuda', index=0)
+    test_module: Linear = Linear(in_features=1, out_features=2, bias=True)
--- tests/test_code_generate_.py
+++ tests/test_code_generate_.py
@@ -2,19 +2,35 @@ import argparse
 import unittest
 from pathlib import Path
 
+import torch
+
 from fucking_argparse import gen_codes
 
 
-class TestStringMethods(unittest.TestCase):
+class TestGenMethods(unittest.TestCase):
     def test_gen_codes(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument("--inductor", type=str, default="rule")
-        parser.add_argument("--group_beam", type=bool, default=False)
-        parser.add_argument("--mlm_training", type=bool, default=False)
-        parser.add_argument("--bart_training", type=bool, default=False)
-        parser.add_argument("--if_then", type=bool, default=False)
-        parser.add_argument("--task", type=str, default="openrule155")
-        gen_args = gen_codes(args=parser.parse_args())
+        parser.add_argument("--test_str", type=str, default="rule")
+        parser.add_argument("--test_bool", type=bool, default=False)
+        parser.add_argument("--test_int", type=int, default=123)
+        parser.add_argument("--test_float", type=float, default=3.1)
+        parser.add_argument("--test_complex", type=complex, default=1 + 2j)
+        parser.add_argument("--test_list", type=list, default=[1, 2.1, "hello", False])
+        parser.add_argument(
+            "--test_set", type=complex, default={1, 2.1, "hello", False}
+        )
+        parser.add_argument(
+            "--test_dict",
+            type=complex,
+            default={"a": 1, "b": 2.1, "c": "hello", "d": False},
+        )
+        parser.add_argument("--test_path", type=Path, default=Path("."))
+        parser.add_argument("--test_device", type=torch.device, default=torch.device(0))
+        parser.add_argument(
+            "--test_module", type=torch.nn.Module, default=torch.nn.Linear(1, 2)
+        )
+        # gen_codes(args=parser.parse_args(), file_path="./arguments.py", exist_ok=True)
+        gen_args = gen_codes(args=parser.parse_args(), class_name="Arguments")
 
         with open(Path(__file__).parent / "arguments.py") as file:
             self.assertEqual(gen_args, file.read())
